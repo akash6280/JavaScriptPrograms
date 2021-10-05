f8729d945b9d0faf001559516f696eed3c7f8941
Akash Singh <akashsingh6280@gmail.com>
2021-10-06 00:56:05 +0530

    ADDED: finding sum and average of five two digit number

--- Sequence_Practice/sequence_programs.js
+++ Sequence_Practice/sequence_programs.js
@@ -8,3 +8,10 @@ let diceNumber1 = 1 + Math.floor(Math.random() * 10)%6;
 let diceNumber2 = 1 + Math.floor(Math.random() * 10)%6;
 let sum=diceNumber1+diceNumber2;
 console.log("sum is "+ sum); 
+
+sum = 0;
+for(let count=1; count<=5;count++)
+    sum += Math.floor(Math.random() * 100);
+console.log("sum is "+ sum);
+let avg = sum/5;
+console.log("average is "+avg); 
