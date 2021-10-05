5768f4044f2da086e5412220ff1a27393ec72349
Akash Singh <akashsingh6280@gmail.com>
2021-10-06 00:49:58 +0530

    ADDED: printing sum of two dice number

--- Sequence_Practice/sequence_programs.js
+++ Sequence_Practice/sequence_programs.js
@@ -3,3 +3,8 @@ console.log("Digit is "+digit);
 
 let diceNumber = 1 + Math.floor(Math.random() * 10)%6;
 console.log("Dice number is = "+diceNumber);
+
+let diceNumber1 = 1 + Math.floor(Math.random() * 10)%6;
+let diceNumber2 = 1 + Math.floor(Math.random() * 10)%6;
+let sum=diceNumber1+diceNumber2;
+console.log("sum is "+ sum); 
