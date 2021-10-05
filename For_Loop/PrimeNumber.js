48dc080cc89d26352d0bc3d1e9147f24b1eaad3d
Akash Singh <akashsingh6280@gmail.com>
2021-10-06 04:13:34 +0530

    MODIFIED: to print prime number between a given range

--- For_Loop/PrimeNumber.js
+++ For_Loop/PrimeNumber.js
@@ -1,14 +1,13 @@
 const args = process.argv.slice(2);
-let number = parseInt(args[0]);
-
+let lowLimit = parseInt(args[0]);
+let highLimit= parseInt(args[1]);
 let flag = true;
-
-for (let factor = 2; factor<= Math.sqrt(number); factor++) {
-    if (number % factor == 0)
-        flag=false;
-}
-
+for(let number=lowLimit;number<=highLimit;number++){
+    flag=true;
+    for (let factor = 2; factor<= Math.sqrt(number); factor++) {
+      if (number % factor == 0)
+         flag=false;
+    }
 if (flag)
-    console.log("Number is prime");
-else
-    console.log("Number is not prime");
+    console.log(number);
+}
