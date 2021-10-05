e013a9669bf0cc8fa09da73c56c336e092f8e085
Akash Singh <akashsingh6280@gmail.com>
2021-10-06 02:36:44 +0530

    ADDED: program to read a number and print it in words

---
+++ If_Else_If/NumberInWord.js
@@ -0,0 +1,24 @@
+const args = process.argv.slice(2);
+let number = parseInt(args[0]);
+
+if (number == 1) {
+    console.log("One");
+} else if (number == 2) {
+    console.log("Two");
+} else if (number == 3) {
+    console.log("Three");
+} else if (number == 4) {
+    console.log("Four");
+} else if (number == 5) {
+    console.log("Five");
+} else if (number == 6) {
+    console.log("Six");
+} else if (number == 7) {
+    console.log("Seven");
+} else if (number == 8) {
+    console.log("Eight");
+} else if (number == 9) {
+    console.log("Nine");
+} else {
+    console.log("Wrong number");
+}
