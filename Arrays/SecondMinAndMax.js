f1582a9555aacd9c54926492d4a03d224a29eef3
Akash Singh <akashsingh6280@gmail.com>
2021-10-07 02:00:37 +0530

    REFACTORED: Finding second minimum & maximum using sort function

--- Arrays/SecondMinAndMax.js
+++ Arrays/SecondMinAndMax.js
@@ -1,24 +1,14 @@
 let numberArray = new Array();
 
-var firstMinimumValue=Number.MAX_VALUE;
-var secondMinimumValue=Number.MAX_VALUE;
-
-var firstMaximumvalue=Number.MIN_VALUE;
+var secondMinimumValue=Number.MAX_VALUE
 var secondMaximumvalue=Number.MIN_VALUE; 
 
 for(let index=0;index<10;index++){
     let number=Math.floor(Math.random()*(1000));
     numberArray.push(number);
-    firstMaximumvalue=Math.max(firstMaximumvalue,numberArray[index]);
-    firstMinimumValue=Math.min(firstMinimumValue,numberArray[index])
 }
-
-for(let index=0;index<10;index++){
-    if (numberArray[index] != firstMaximumvalue)
-        secondMaximumvalue = Math.max(secondMaximumvalue,numberArray[index]);
-    if (numberArray[index] != firstMinimumValue)
-        secondMinimumValue = Math.min(secondMinimumValue,numberArray[index]);
-}
-
+numberArray.sort(function(a, b){return a - b});
+secondMinimumValue=numberArray[1];
+secondMaximumvalue=numberArray[numberArray.length-2];
 console.log("Second minimum number = "+secondMinimumValue);
 console.log("Second maximum number = "+secondMaximumvalue);
